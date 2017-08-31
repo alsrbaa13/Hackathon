463653900142ad80821b9d6a802ee70b03187608
MinGyu <alsrbaa13@naver.com>
2017-08-31 22:02:34 +0900

    GyroSensor

---
+++ Android/MinGyu/GyroSensor/app/src/main/java/com/example/dsm2016/gyrosensor/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.dsm2016.gyrosensor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //Using the Accelometer & Gyroscoper
+    private SensorManager mSensorManager = null;
+
+    //Using the Gyroscope
+    private SensorEventListener mGyroLis;
+    private Sensor mGgyroSensor = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Using the Gyroscope & Accelometer
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        //Using the Accelometer
+        mGgyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mGyroLis = new PositionManager();
+
+        //Touch Listener for Accelometer
+        findViewById(R.id.a_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSensorManager.registerListener(mGyroLis, mGgyroSensor, SensorManager.SENSOR_DELAY_UI);
+            }
+        });
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.e("LOG", "onPause()");
+        mSensorManager.unregisterListener(mGyroLis);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.e("LOG", "onDestroy()");
+        mSensorManager.unregisterListener(mGyroLis);
+    }
+
+}
